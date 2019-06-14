cd7f01442cdd138500e301226c6c5d9c59dec632
Seongjoo Jin <evanjin8@gmail.com>
2019-06-14 21:17:59 +0900

    front 초기 세팅

---
+++ front/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    parser: 'babel-eslint',
+    parser: '@typescript-eslint/parser'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/vue'
+  ],
+  plugins: [
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+    '@typescript-eslint/no-unused-vars': 0
+  },
+};
