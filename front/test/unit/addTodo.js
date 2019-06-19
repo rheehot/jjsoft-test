27d62b4d323659752e944627d0045849ae6da643
evanjin <evanjin8@gmail.com>
2019-06-19 15:36:37 +0900

    [FB] todo 검색 추가

--- front/test/unit/addTodo.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai'
-import IndexPage from '../../pages/index.vue'
-
-describe('Todo Test', () => {
-  it('Todo test', () => {
-    expect(IndexPage.todoItems).to.have.lengthOf(20)
-  })
-})
